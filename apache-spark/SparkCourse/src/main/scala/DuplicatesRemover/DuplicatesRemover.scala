8220eb28c0bb316da602af426f6cd8f2de897224
Fei Zhan <fei@loypal.io>
2021-01-05 11:55:29 +1000

    learned how to run it in teriminal

--- apache-spark/SparkCourse/src/main/scala/DuplicatesRemover/DuplicatesRemover.scala
+++ apache-spark/SparkCourse/src/main/scala/DuplicatesRemover/DuplicatesRemover.scala
@@ -1,7 +1,13 @@
 package DuplicatesRemover
+import scala.io.Source
 
 object DuplicatesRemover {
   def main(args: Array[String]): Unit = {
-
+    val filename: String = args(0)
+    val textSource = Source.fromFile(filename)
+    val fileContents: String = textSource.mkString
+    textSource.close()
+    println(fileContents)
+    // println(System.getProperty("user.dir"))
   }
 }
