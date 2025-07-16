74ec8ad32d90601b7260fe71cab0f6eb95b71112
Fei Zhan <f.zhan@uq.edu.au>
2025-07-16 10:35:23 +1000

    added rust hello

---
+++ rust/hello/target/debug/deps/hello-7f48df267e7c8145.d
@@ -0,0 +1,5 @@
+/home/fei/Documents/golden_rush/rust/hello/target/debug/deps/hello-7f48df267e7c8145.d: src/main.rs
+
+/home/fei/Documents/golden_rush/rust/hello/target/debug/deps/hello-7f48df267e7c8145: src/main.rs
+
+src/main.rs:
---
+++ rust/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/fei/Documents/golden_rush/rust/hello/target/debug/hello: /home/fei/Documents/golden_rush/rust/hello/src/main.rs
